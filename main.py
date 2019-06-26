fe66a0d22bc3b53d39f712a4612168f30b02bed6
PythonBit <52246792+PythonBit@users.noreply.github.com>
2019-06-26 20:11:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ import Modules.leaveserver as leaveserver
 import Modules.floodchat as floodchat
 import Modules.sendmessage as sendmessage
 import Modules.deleteserver as deleteserver
+import Modules.banuser as banuser
 
-ctypes.windll.kernel32.SetConsoleTitleW("Goldiscord V1.8")
+ctypes.windll.kernel32.SetConsoleTitleW("Goldiscord V1.9")
 green = colorama.Fore.GREEN
 normal = colorama.Style.RESET_ALL
 colorama.init()
 
 token = input("Authorization Token: ")
 def main():
-    print("__==Goldiscord V1.8==__")
-    option = input("\n1) Server Spam\n2) Block Friend\n3) Leave Server\n4) Flood Chat\n5) Send Message\n6) Delete Server\n>")
+    print("__==Goldiscord V1.9==__")
+    option = input("\n1) Server Spam\n2) Block Friend\n3) Leave Server\n4) Flood Chat\n5) Send Message\n6) Delete Server\n7) Ban User\n>")
 
     if option == "1":
         print("Loaded module:"+green,"Server Spam"+normal)
@@ -67,5 +68,14 @@ def main():
         deleteserver.delete_server(token, server_id)
         os.system("cls")
         print("Successfully executed module: " + green + "Delete Server"+normal)
+        main()
+
+    if option == "7":
+        print("Loaded module:"+green,"Ban User"+normal)
+        server_id = input("Server ID: ")
+        user_id = input("User ID: ")
+        banuser.ban_user(token, server_id, user_id)
+        os.system("cls")
+        print("Successfully executed module: " + green + "Ban User"+normal)
         main()
 main()
